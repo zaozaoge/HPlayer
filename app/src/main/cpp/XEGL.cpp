2acebd79a288f3482f428f8d9d51b9ae31568c46
huzhangxiao <1402381090@qq.com>
2019-05-26 12:07:51 +0800

    XEGL的线程安全处理和清理函数

--- app/src/main/cpp/XEGL.cpp
+++ app/src/main/cpp/XEGL.cpp
@@ -6,6 +6,7 @@
 #include "XEGL.h"
 #include "XLog.h"
 #include <EGL/egl.h>
+#include <mutex>
 
 class CXEGL : public XEGL {
 
@@ -14,28 +15,59 @@ public:
     EGLDisplay display = EGL_NO_DISPLAY;
     EGLSurface surface = EGL_NO_SURFACE;
     EGLContext context = EGL_NO_CONTEXT;
+    std::mutex mutex;
 
     virtual void Draw() {
+        mutex.lock();
         if (display == EGL_NO_DISPLAY || surface == EGL_NO_SURFACE) {
+            mutex.unlock();
             return;
         }
         eglSwapBuffers(display, surface);
+        mutex.unlock();
     }
 
+    virtual void Close() {
+        mutex.lock();
+        if (display == EGL_NO_DISPLAY) {
+            mutex.unlock();
+            return;
+        }
+        //去除绑定关系
+        eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+        if (surface != EGL_NO_SURFACE) {
+            //销毁surface
+            eglDestroySurface(display, surface);
+        }
+        if (context != EGL_NO_CONTEXT) {
+            //销毁context
+            eglDestroyContext(display, context);
+        }
+        //中止display
+        eglTerminate(display);
+        display = EGL_NO_DISPLAY;
+        surface = EGL_NO_SURFACE;
+        context = EGL_NO_CONTEXT;
+        mutex.unlock();
+    }
 
     virtual bool Init(void *win) {
+        Close();
         auto window = static_cast<ANativeWindow *>(win);
         //初始化EGL
-
+        mutex.lock();
         //1、获取EGLDisplay对象，显示设备
         display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
         if (display == EGL_NO_DISPLAY) {
             XLogi("eglGetDisplay failed");
+            mutex.unlock();
+            return false;
         }
         XLogi("eglGetDisplay success");
         //2、初始化Display
         if (EGL_TRUE != eglInitialize(display, 0, 0)) {
             XLogi("eglInitialize failed");
+            mutex.unlock();
             return false;
         }
         XLogi("eglInitialize success");
@@ -51,6 +83,7 @@ public:
         EGLint numConfigs = 0;
         if (EGL_TRUE != eglChooseConfig(display, configSpec, &config, 1, &numConfigs)) {
             XLogi("eglChooseConfig failed");
+            mutex.unlock();
             return false;
         }
         XLogi("eglChooseConfig success");
@@ -63,14 +96,17 @@ public:
         context = eglCreateContext(display, config, EGL_NO_CONTEXT, ctxAttr);
         if (context == EGL_NO_CONTEXT) {
             XLogi("eglCreateContext failed");
+            mutex.unlock();
             return false;
         }
         XLogi("eglCreateContext success");
         if (EGL_TRUE != eglMakeCurrent(display, surface, surface, context)) {
             XLogi("eglMakeCurrent failed");
+            mutex.unlock();
+            return false;
         }
         XLogi("eglMakeCurrent success");
-
+        mutex.unlock();
         return true;
     }
 };
@@ -79,5 +115,7 @@ XEGL *XEGL::Get() {
     static CXEGL egl;
     return &egl;
 }
+
+
 
 
